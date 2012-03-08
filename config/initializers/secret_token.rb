b574981c4efacee143382bd99e8ee1edd47e57fe
Jia Wu <wujia123@yahoo.com>
2012-03-08 16:26:41 -0500

    rails set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boxy::Application.config.secret_token = '2df9ad67b02f8b5ccfe0e6c4430d0ec61c8819d726129a79e36a798abbeafde875db16ee8646ad496251cb3f47a6a0be2cef5d4f8f4363350f9fc59400a222a8'
